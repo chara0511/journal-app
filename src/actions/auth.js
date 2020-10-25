c8e4b2f1027ffaecffc693e55b195a17514de965
Chara- <escor05_11@hotmail.com>
2020-10-25 01:21:07 -0500

    applymiddleware redux-thunk

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,6 +1,12 @@
 import { LOG_IN } from "../types";
 
-export const logIn = (uid, displayName) => ({
+export const logIn = (value1, value2) => (dispatch) => {
+  setTimeout(() => {
+    dispatch(sendData(value1, value2));
+  }, 3000);
+};
+
+export const sendData = (uid, displayName) => ({
   type: LOG_IN,
   payload: { uid, displayName },
 });
--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,17 @@
-import { combineReducers, createStore } from "redux";
+import { combineReducers, createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+
 import { authReducer } from "../reducers/authReducer";
+
+const composeEnhancers =
+  (typeof window !== "undefined" &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
 
 const reducers = combineReducers({
   auth: authReducer,
 });
 
-const store = createStore(
-  reducers,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
 
 export default store;
